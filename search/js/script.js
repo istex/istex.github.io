25a52436ea04911b6bfd19e16370f1fada17006f
Stéphane Gully <stephane.gully@gmail.com>
2014-12-09 10:30:45 +0100

    update dist

--- search/js/script.js
+++ search/js/script.js
@@ -45,12 +45,15 @@
     // connect the submit action
     $(self.elt).find('.istex-search-form').submit(function () {
 
+      var query = $(self.elt).find('input.istex-search-input').val().trim();
+      query = query ? query : '*';
+
       // send the request to the istex api with the
       // jquery-jsonp lib because errors are not
       // handled by the native jquery jsonp function
       $.jsonp({
         url: self.settings.istexApi + '/document/',
-        data: { q: $(self.elt).find('input.istex-search-input').val() },
+        data: { q: query },
         callbackParameter: "callback",
         success: function(items) {
           console.log(items);
@@ -62,7 +65,9 @@
           $.event.trigger('istex-results', [ self, items ]);
         },
         error: function (opt, err) {
-          $(self.elt).find('.istex-search-error').html('<a href="https://api.istex.fr/document/?q=*">API Istex</a> non joignable.');
+          $(self.elt).find('.istex-search-error').html(
+            '<a href="https://api.istex.fr/document/?q=*">API Istex</a> non joignable.'
+          );
           $(self.elt).find('.istex-search-error').show();
         }
       });
