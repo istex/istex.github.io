6c1d0bc15ba15d46fb719de24550b9e74c4f2051
Stéphane Gully <stephane.gully@gmail.com>
2015-02-04 17:18:50 +0100

    version des widgets

---
+++ latest/istexconfigdefault.js
@@ -0,0 +1,51 @@
+var istexConfigDefault = {
+  // l'adresse de l'API de l'Istex
+  // pour une ezproxyfication, réglez ici l'adresse ezproxyfiée
+  // ex à l'UL: https://api-istex-fr.bases-doc.univ-lorraine.fr 
+  istexApi: 'https://api.istex.fr',
+  
+  // pour lancer une recherche au chargement de la page
+  // indiquer les mots à rechercher (argument de ?q= au niveau de l'api istex)
+  query: "",
+
+  // il est possible de cacher la zone de pagination avec ce paramètre
+  showPagination: true,
+
+  // nombre de résultats souhaités par page
+  pageSize: 10,
+
+  // nombre max de pages à montrer dans la zone de pagination
+  maxPagesInPagination: 10,
+
+  // le nombre max de caractères du résumé à afficher
+  abstractLength: 250,
+
+  // le nombre max de caractères du titre à afficher
+  titleLength: 100,
+
+  // le format qu'on souhaite voir s'ouvrir quand on clique sur le titre
+  fullTextOnTitle: 'pdf',
+  
+  // il est possible de cacher l'affichage de la vitesse de la requête
+  // ex: "Environ 8 933 993 résultats (0.24 secondes)"
+  //     si showQuerySpeed vaut false, "(0.24 secondes)" ne sera pas affiché
+  showQuerySpeed: true,
+
+  // le nom de l'évènement émit au moment de l'authentification réussie
+  connectedEventName: "istex-connected",
+
+  // le nom de l'évènement émit au moment d'une recherche    
+  resultsEventName: "istex-results",
+
+  // le nom de l'évènement émit au moment d'un changement de page
+  gotoPageEventName: "istex-gotopage",
+
+  // le nom de l'évènement émit a chaque fois qu'une recherche est envoyée
+  // et qui donnera probablement (sauf erreur) lieux à un event "istex-results"
+  waitingForResultsEventName: "istex-waiting-for-results"
+};
+
+// create a empty istexConfig variable
+if (!istexConfig) {
+  var istexConfig = {};
+}
