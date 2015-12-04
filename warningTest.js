d0da700b1731a53334578f5f1b9b4fb37318da77
thomasfrantz <thom.frantz@gmail.com>
2015-12-04 10:40:38 +0100

    updateAPI3.2

--- warningTest.js
+++ warningTest.js
@@ -1,9 +1,9 @@
 var warning = ''+
-'<div class="blog-header alert alert-warning" style="padding: 5px;">'+
-	'<h4 class="blog-title" style="display:inline-block;margin-bottom:0px;">'+
+'<div class="blog-header alert alert-warning" style="padding:5px; width:70%; margin-left:15%;">'+
+	'<h4 class="blog-title" style="display:inline-block;margin-bottom:10px;">'+
 		'Information'+
 	'</h4>'+
-	'<div id="widget-istex-info-arrow" class="icon arrow" style="cursor: pointer;display: inline-block;background: transparent url(&quot;https://istex.github.io/img/arrow-d.png&quot;) no-repeat scroll 0% 0% / 20px 20px; width: 20px; height: 20px; margin-left: 10px; vertical-align: bottom;"></div>'+
+	'<div id="widget-istex-info-arrow" class="icon arrow" style="cursor: pointer; display: inline-block; background: transparent url(&quot;https://istex.github.io/img/arrow-d.png&quot;) no-repeat scroll 0% 0% / 20px 20px; width: 20px; height: 20px; margin-left: 10px; vertical-align: bottom; margin-bottom: 10px;"></div>'+
 	'<p id="widget-istex-info" class="widget-istex-info blog-description" style="height:100%;">'+
 		'Cette interface a été créée dans le cadre du projet ISTEX, dont le but est de mettre à disposition de la communauté de l’enseignement supérieur et de la recherche un ensemble de documents scientifiques dans toutes les disciplines.<br>'+
 		'Un projet annexe a été confié à la Direction du Numérique de l\'Université de Lorraine : ISTEX-SNU (ISTEX intégré aux Services Numériques des Usagers). Ce projet apporte une interface facile d\'utilisation pour la platforme ISTEX et s\'intègre sans difficulté dans les environnements de travail comme les ENT (Espace Numérique de Travail) ou les DT (Discovery Tools).<br>'+
